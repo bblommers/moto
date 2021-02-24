fac9241a9cfe2dffac1e44079abb7bffd18a596c
Bert Blommers <info@bertblommers.nl>
2021-02-24 15:34:03 +0000

    Change action to composite, instead of Docker

--- .github/actions/moto-release/entrypoint.sh
+++ .github/actions/moto-release/entrypoint.sh
@@ -2,4 +2,10 @@
 
 echo "Hello $1"
 time=$(date)
+pwd
+ls -la /github/workspace
+# Verify that our version does not yet exist
+# Verify that the Changelog contains our version
+# Update moto/__init__.py to set the version
+
 echo "::set-output name=time::$time"
