edd91923ae56967d1b7c28ce62059358b101be3f
Bert Blommers <info@bertblommers.nl>
2022-10-25 09:34:40 +0000

    TEST: latest botocore

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ def get_version():
 
 
 install_requires = [
-    "boto3>=1.9.201",
-    "botocore>=1.12.201",
+    "boto3>=1.28.0",
+    "botocore>=1.28.0",
     "cryptography>=3.3.1",
     "requests>=2.5",
     "xmltodict",
