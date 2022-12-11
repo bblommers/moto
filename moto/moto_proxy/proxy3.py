16356a4d7d5ac3a94df289985af2ee75c1e976c2
Bert Blommers <info@bertblommers.nl>
2022-12-11 21:19:12 -0100

    Intercept calls and redirect to Moto

--- moto/moto_proxy/proxy3.py
+++ moto/moto_proxy/proxy3.py
@@ -20,19 +20,59 @@ from io import StringIO
 from subprocess import Popen, PIPE
 import html
 from html.parser import HTMLParser
+from threading import Lock
+
+from botocore.awsrequest import AWSPreparedRequest
+from moto.backends import get_backend
+from moto.backend_index import backend_url_patterns
+from moto.core import BackendDict, DEFAULT_ACCOUNT_ID
 
 # Adapted from https://github.com/xxlv/proxy3
 
 
-def with_color(c, s):
-    return "\x1b[%dm%s\x1b[0m" % (c, s)
+class MotoRequestHandler:
+
+    def __init__(self):
+        self.lock = Lock()
+
+    def get_backend_for_host(self, host):
+        for backend, pattern in backend_url_patterns:
+            if pattern.match(host):
+                return backend
+
+    def get_handler_for_host(self, host: str, path: str):
+        backend_name = self.get_backend_for_host(host)
+        backend_dict = get_backend(backend_name)
+
+        # Get an instance of this backend.
+        # We'll only use this backend to resolve the URL's, so the exact region/account_id is irrelevant
+        if isinstance(backend_dict, BackendDict):
+            if "us-east-1" in backend_dict[DEFAULT_ACCOUNT_ID]:
+                backend = backend_dict[DEFAULT_ACCOUNT_ID]["us-east-1"]
+            else:
+                backend = backend_dict[DEFAULT_ACCOUNT_ID]["global"]
+        else:
+            backend = backend_dict["global"]
+
+        for url_path, handler in backend.flask_paths.items():
+            if re.match(url_path, path):
+                return handler
+        return None
+
+    def parse_request(self, method, host, path, headers, body: bytes):
+        with self.lock:
+            handler = self.get_handler_for_host(host=host, path=path)
+            full_url = host + path
+            request = AWSPreparedRequest(method, full_url, headers, body, stream_output=False)
+            return handler(request=request, full_url=full_url, headers=headers)
+
+
+
+MOTO_REQUEST_HANDLER = MotoRequestHandler()
 
 
 def join_with_script_dir(path):
-    print(f"join_with_script_dir({path}")
-    resp = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    print(resp)
-    return resp
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
 
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
@@ -61,13 +101,6 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         self.tls.conns = {}
 
         BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
-
-    def log_error(self, format, *args):
-        # surpress "Request timed out: timeout('timed out',)"
-        if isinstance(args[0], socket.timeout):
-            return
-
-        self.log_message(format, *args)
 
     def do_CONNECT(self):
         if os.path.isfile(self.cakey) and os.path.isfile(self.cacert) and os.path.isfile(
@@ -135,82 +168,32 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         req = self
         content_length = int(req.headers.get('Content-Length', 0))
         req_body = self.rfile.read(content_length) if content_length else None
-
-        if req.path[0] == '/':
-            if isinstance(self.connection, ssl.SSLSocket):
-                req.path = "https://%s%s" % (req.headers['Host'], req.path)
-            else:
-                req.path = "http://%s%s" % (req.headers['Host'], req.path)
-
-        req_body_modified = self.request_handler(req, req_body)
-        if req_body_modified is False:
-            self.send_error(403)
-            return
-        elif req_body_modified is not None:
-            req_body = req_body_modified
-            req.headers['Content-length'] = str(len(req_body))
-
-        u = urlsplit(req.path)
-        scheme, netloc, path = u.scheme, u.netloc, (u.path + '?' + u.query if u.query else u.path)
-        assert scheme in ('http', 'https')
-        if netloc:
-            req.headers['Host'] = netloc
-        setattr(req, 'headers', self.filter_headers(req.headers))
+        if isinstance(self.connection, ssl.SSLSocket):
+            host = "https://" + req.headers['Host']
+        else:
+            host = "http://" + req.headers['Host']
+        path = req.path
 
         try:
-            origin = (scheme, netloc)
-            if not origin in self.tls.conns:
-                if scheme == 'https':
-                    self.tls.conns[origin] = httplib.HTTPSConnection(netloc, timeout=self.timeout)
-                else:
-                    self.tls.conns[origin] = httplib.HTTPConnection(netloc, timeout=self.timeout)
-            conn = self.tls.conns[origin]
-            conn.request(self.command, path, req_body, dict(req.headers))
-            res = conn.getresponse()
+            res_status, res_headers, res_body = MOTO_REQUEST_HANDLER.parse_request(method=req.command, host=host, path=path, headers=req.headers, body=req_body)
+            res_body = res_body.encode("utf-8")
+            res_version = "HTTP/1.1"
+            res_reason = "OK"
 
-            version_table = {10: 'HTTP/1.0', 11: 'HTTP/1.1'}
-            setattr(res, 'headers', res.msg)
-            setattr(res, 'response_version', version_table[res.version])
-
-            # support streaming
-            if not 'Content-Length' in res.headers and 'no-store' in res.headers.get('Cache-Control', ''):
-                self.response_handler(req, req_body, res, '')
-                setattr(res, 'headers', self.filter_headers(res.headers))
-                self.relay_streaming(res)
-                with self.lock:
-                    self.save_handler(req, req_body, res, '')
-                return
-
-            res_body = res.read()
         except Exception as e:
-            if origin in self.tls.conns:
-                del self.tls.conns[origin]
+            print(e)
             self.send_error(502)
             return
 
-        content_encoding = res.headers.get('Content-Encoding', 'identity')
-        res_body_plain = self.decode_content_body(res_body, content_encoding)
+        content_encoding = res_headers.get('Content-Encoding', 'identity')
+        res_headers['Content-Length'] = str(len(res_body))
 
-        res_body_modified = self.response_handler(req, req_body, res, res_body_plain)
-        if res_body_modified is False:
-            self.send_error(403)
-            return
-        elif res_body_modified is not None:
-            res_body_plain = res_body_modified
-            res_body = self.encode_content_body(res_body_plain, content_encoding)
-            res.headers['Content-Length'] = str(len(res_body))
-
-        setattr(res, 'headers', self.filter_headers(res.headers))
-
-        self.wfile.write(f"{self.protocol_version} {res.status} {res.reason}\r\n".encode("utf-8"))
-        for k, v in res.headers.items():
+        self.wfile.write(f"{self.protocol_version} {res_status} {res_reason}\r\n".encode("utf-8"))
+        for k, v in res_headers.items():
             self.send_header(k, v)
         self.end_headers()
         self.wfile.write(res_body)
         self.wfile.flush()
-
-        with self.lock:
-            self.save_handler(req, req_body, res, res_body_plain)
 
     def relay_streaming(self, res):
         self.wfile.write("%s %d %s\r\n" % (self.protocol_version, res.status, res.reason))
@@ -234,52 +217,6 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
     do_DELETE = do_GET
     do_OPTIONS = do_GET
 
-    def filter_headers(self, headers):
-        # http://tools.ietf.org/html/rfc2616#section-13.5.1
-        hop_by_hop = (
-        'connection', 'keep-alive', 'proxy-authenticate', 'proxy-authorization', 'te', 'trailers', 'transfer-encoding',
-        'upgrade')
-        for k in hop_by_hop:
-            del headers[k]
-
-        # accept only supported encodings
-        if 'Accept-Encoding' in headers:
-            ae = headers['Accept-Encoding']
-            filtered_encodings = [x for x in re.split(r',\s*', ae) if x in ('identity', 'gzip', 'x-gzip', 'deflate')]
-            headers['Accept-Encoding'] = ', '.join(filtered_encodings)
-
-        return headers
-
-    def encode_content_body(self, text, encoding):
-        if encoding == 'identity':
-            data = text
-        elif encoding in ('gzip', 'x-gzip'):
-            io = StringIO()
-            with gzip.GzipFile(fileobj=io, mode='wb') as f:
-                f.write(text)
-            data = io.getvalue()
-        elif encoding == 'deflate':
-            data = zlib.compress(text)
-        else:
-            raise Exception("Unknown Content-Encoding: %s" % encoding)
-        return data
-
-    def decode_content_body(self, data, encoding):
-        if encoding == 'identity':
-            text = data
-        elif encoding in ('gzip', 'x-gzip'):
-            io = StringIO(data)
-            with gzip.GzipFile(fileobj=io) as f:
-                text = f.read()
-        elif encoding == 'deflate':
-            try:
-                text = zlib.decompress(data)
-            except zlib.error:
-                text = zlib.decompress(data, -zlib.MAX_WBITS)
-        else:
-            raise Exception("Unknown Content-Encoding: %s" % encoding)
-        return text
-
     def send_cacert(self):
         with open(self.cacert, 'rb') as f:
             data = f.read()
@@ -290,94 +227,6 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Connection', 'close')
         self.end_headers()
         self.wfile.write(data)
-
-    def print_info(self, req, req_body, res, res_body):
-        def parse_qsl(s):
-            return '\n'.join("%-20s %s" % (k, v) for k, v in urllib.parse.parse_qsl(s, keep_blank_values=True))
-
-        req_header_text = "%s %s %s\n%s" % (req.command, req.path, req.request_version, req.headers)
-        res_header_text = "%s %d %s\n%s" % (res.response_version, res.status, res.reason, res.headers)
-
-        print(with_color(33, req_header_text))
-
-        u = urlsplit(req.path)
-        if u.query:
-            query_text = parse_qsl(u.query)
-            print(with_color(32, "==== QUERY PARAMETERS ====\n%s\n" % query_text))
-
-        cookie = req.headers.get('Cookie', '')
-        if cookie:
-            cookie = parse_qsl(re.sub(r';\s*', '&', cookie))
-            print(with_color(32, "==== COOKIE ====\n%s\n" % cookie))
-
-        auth = req.headers.get('Authorization', '')
-        if auth.lower().startswith('basic'):
-            token = auth.split()[1].decode('base64')
-            print(with_color(31, "==== BASIC AUTH ====\n%s\n" % token))
-
-        if req_body is not None:
-            req_body_text = None
-            content_type = req.headers.get('Content-Type', '')
-
-            if content_type.startswith('application/x-www-form-urlencoded'):
-                req_body_text = parse_qsl(req_body)
-            elif content_type.startswith('application/json'):
-                try:
-                    json_obj = json.loads(req_body)
-                    json_str = json.dumps(json_obj, indent=2)
-                    if json_str.count('\n') < 50:
-                        req_body_text = json_str
-                    else:
-                        lines = json_str.splitlines()
-                        req_body_text = "%s\n(%d lines)" % ('\n'.join(lines[:50]), len(lines))
-                except ValueError:
-                    req_body_text = req_body
-            elif len(req_body) < 1024:
-                req_body_text = req_body
-
-            if req_body_text:
-                print(with_color(32, "==== REQUEST BODY ====\n%s\n" % req_body_text))
-
-        print(with_color(36, res_header_text))
-
-        cookies = res.headers.get('Set-Cookie')
-        if cookies:
-            cookies = '\n'.join(cookies)
-            print(with_color(31, "==== SET-COOKIE ====\n%s\n" % cookies))
-
-        if res_body is not None:
-            res_body_text = None
-            content_type = res.headers.get('Content-Type', '')
-
-            if content_type.startswith('application/json'):
-                try:
-                    json_obj = json.loads(res_body)
-                    json_str = json.dumps(json_obj, indent=2)
-                    if json_str.count('\n') < 50:
-                        res_body_text = json_str
-                    else:
-                        lines = json_str.splitlines()
-                        res_body_text = "%s\n(%d lines)" % ('\n'.join(lines[:50]), len(lines))
-                except ValueError:
-                    res_body_text = res_body
-            elif content_type.startswith('text/html'):
-                m = re.search(rb'<title[^>]*>\s*([^<]+?)\s*</title>', res_body, re.I)
-                if m:
-                    print(with_color(32, "==== HTML TITLE ====\n%s\n" % html.unescape(m.group(1).decode('utf-8'))))
-            elif content_type.startswith('text/') and len(res_body) < 1024:
-                res_body_text = res_body
-
-            if res_body_text:
-                print(with_color(32, "==== RESPONSE BODY ====\n%s\n" % res_body_text))
-
-    def request_handler(self, req, req_body):
-        pass
-
-    def response_handler(self, req, req_body, res, res_body):
-        pass
-
-    def save_handler(self, req, req_body, res, res_body):
-        self.print_info(req, req_body, res, res_body)
 
 
 def main(HandlerClass=ProxyRequestHandler, ServerClass=ThreadingHTTPServer, protocol="HTTP/1.1"):
